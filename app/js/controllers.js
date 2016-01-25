b27845dcc973a3eedff95a3a440e2c75d77928d0
St. John, Brady <Brady.St.John@cdk.com>
2016-01-25 08:44:04 -0800

    More functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,16 +4,34 @@
 var silanisControllers = angular.module('silanisControllers', []);
 
 silanisControllers.controller('testCtrl', ['$scope', '$sce',
-  function($scope, $sce) {
-      var silanisAPI      = 'https://sandbox.e-signlive.com/access?sessionToken=';
-      $scope.catSharkUrl  = $sce.trustAsResourceUrl('http://i.imgur.com/GXbLRfn.webm');
-      $scope.sessionToken = '';
-      $scope.userInput    = false;
+    function($scope, $sce) {
+        $scope.catSharkUrl  = $sce.trustAsResourceUrl('http://i.imgur.com/GXbLRfn.webm');
+        $scope.sessionToken = '';
+        $scope.packageId    = '';
+        $scope.documentId   = '';
+        $scope.userInput    = false;
 
-      $scope.updateIFrame = function () {
-          $scope.userInput = true;
+        $scope.updateIFrame = function () {
+            var silanisSessionAPI = 'https://sandbox.e-signlive.com/access?sessionToken=';
+            var silanisPackageApi = 'https://sandbox.e-signlive.com/packages/';
+            var silanisDocument   = '/documents/';
+            $scope.userInput = true;
 
-          var element = angular.element('#silanisIFrame');
-          element.attr('src', $sce.trustAsResourceUrl(silanisAPI + $scope.sessionToken));
-      };
-  }]);
+            var element = angular.element('#silanisIFrame');
+            if (!angular.equals($scope.sessionToken, '')) {
+                element.attr('src', $sce.trustAsResourceUrl(silanisSessionAPI + $scope.sessionToken));
+                $scope.sessionToken = '';
+                return;
+            }
+            
+            var apiUrl = silanisPackageApi + $scope.packageId;
+            if(!angular.equals($scope.documentId, '')) {
+                apiUrl += silanisDocument + $scope.documentId;
+            }
+
+            element.attr('src', $sce.trustAsResourceUrl(apiUrl));
+
+            $scope.packageId    = '';
+            $scope.documentId   = '';
+        };
+    }]);
